ac449236026e13d62598e9e5e1a3d6f587f916a0
Nathanial L. McConnell <nate@nmcconnell.com>
2013-08-04 18:39:19 -0400

    initializing rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfi::Application.config.secret_token = '6dde9701262c9706eae6ee729bf7442d79ff7c0ed71afd6a11337f50a48dd1c2625f10b84d7d1b9266b6002ada600bd298f08e30c107fd36782778b8bbe9658e'
