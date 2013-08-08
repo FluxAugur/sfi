fd2a2197a661815870d8ac848816d8ef04eef33c
Nathanial L. McConnell <FluxAugur@gmail.com>
2013-08-08 08:22:54 -0400

    switching server roles in deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
-server 'silenus.sanitaryfabricators.com', :app, :web, :db, :primary => true
+server 'bacchus.sanitaryfabricators.com', :app, :web, :db, :primary => true
 
-# role :web, '192.155.92.211'
-# role :app, '192.155.92.211'
-# role :db,  '192.155.92.211', :primary => true
+# role :web, '74.207.230.150'
+# role :app, '74.207.230.150'
+# role :db,  '74.207.230.150', :primary => true
