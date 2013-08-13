fbd1e058d5c7fc0e03ca4b46233a5566ffebc17b
Nathanial L. McConnell <nate@synaptian.com>
2013-08-13 17:30:16 -0400

    resetting root to

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Sfi::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'products'
+  root :to => 'products#index'
 
   # See how all your routes lay out with "rake routes"
 
