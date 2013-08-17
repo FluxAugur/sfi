ebcb4380b01fdcc3269991be284c1c93a6d8bb32
Nathanial L. McConnell <FluxAugur@gmail.com>
2013-08-16 21:57:28 -0400

    catching routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  rescue_from ActionController::RoutingError, with: :route_not_found
-  rescue_from ActionController::MethodNotAllowed, with: :invalid_method
-
-  private
-
-  def route_not_found
-    redirect_to root_url, notice:  "The page you requested does not exist. Taking you home."
-  end
-
-  def invalid_method
-
-    render text: message, status: :method_not_allowed
-  end
 end
