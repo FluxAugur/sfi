33ba9c02b732f7d4b5a78e42b1b609622db82b2f
Nathanial L. McConnell <FluxAugur@gmail.com>
2013-08-20 23:37:41 -0400

    removing old print_invoice configuration

--- app/models/spree/print_invoice_configuration.rb
+++
@@ -1,8 +0,0 @@
-module Spree
-  class PrintInvoiceConfiguration < Preferences::Configuration
-    #preference :logo, :string, :default => Spree::Config[:logo]
-    Spree::PrintInvoice::Config.set(logo_path: 'sfi/logoSFI.png')
-    preference :suppress_anonymous_address => true
-    preference :print_buttons => "invoice, packing_slip"
-  end
-end
